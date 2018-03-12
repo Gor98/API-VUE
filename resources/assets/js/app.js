bd341ed439187ba649f440614d66de735174d50d
Gor Azizyan <azizyan.gor.98@gmail.com>
2018-03-12 10:21:07 +0000

    added articles via vue

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -17,7 +17,8 @@ window.Vue = require('vue');
 
 
 
-Vue.component('articles', require('/components/Articles.vue'));
+Vue.component('articles', require('./components/Articles.vue'));
+Vue.component('navbar', require('./components/Navbar.vue'));
 
 const app = new Vue({
     el: '#app'
